337609fc7a5b08f8d4aef5c448b6dd9a3bfd521f
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 22:43:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,7 +35,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   array.collect do |food|
-    cheeses = cheese_types.join("")
+    cheeses = cheese_types.join(", ")
     if food.include?(cheeses)
       return nil
     else
