7533db7f951cf3f9de1205ea2990c0d6c4e91e67
Andrew Williams <1williamsaj@gmail.com>
2019-07-25 22:23:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,4 +33,11 @@ def find_the_cheese(array)
   # the array below is here to help
   search_list = array
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  search_list.map do |food|
+    if food.include(cheese_types)
+      p food
+    else
+      return nil
+  end
 end
